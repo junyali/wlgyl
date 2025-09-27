dedc9ba93e31968f66b0d95233e6529bd9aa2ef9
Junya <junjia.liu@pm.me>
2025-09-27 12:10:14 +0100

    Add player stuff

---
+++ scripts/player.gd
@@ -0,0 +1,83 @@
+extends CharacterBody2D
+
+@export var max_speed: float = 40.0 # Maximum speed player is capped at walking
+@export var acceleration: float = 500.0 # From stationary to max speed
+@export var friction: float = 1500.0 # From max speed to stationary
+
+var current_velocity: Vector2  = Vector2.ZERO
+var move_direction: Vector2 = Vector2.ZERO
+var is_action: bool = false
+var last_hit_time: float = 0.0
+
+@onready var sprite: Sprite2D = $Sprite
+@onready var action_area: Area2D = $Range
+@onready var action_timer: Timer = $Action
+
+func _ready() -> void:
+	add_to_group("player")
+	sprite.modulate = Color.WHITE
+
+func _physics_process(delta: float) -> void:
+	handle_movement(delta)
+	handle_action()
+	
+func _process(_delta: float) -> void:
+	pass
+	
+func handle_movement(delta: float) -> void:
+	var h_dir: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var v_dir: float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	var input_dir: Vector2 = Vector2(h_dir, v_dir).normalized()
+	
+	if abs(h_dir) > abs(v_dir):
+		input_dir = Vector2(sign(h_dir), 0)
+	elif abs(v_dir) > 0:
+		input_dir = Vector2(0, sign(v_dir))
+	
+	# Accelerative speed (was that even a word??)
+	if input_dir != Vector2.ZERO:
+		move_direction = input_dir
+	else:
+		move_direction = Vector2.ZERO  # Instant stop
+		
+	# Update facing direction when moving
+	if move_direction != Vector2.ZERO and not is_action:
+		var target_speed: float = max_speed
+		current_velocity = velocity.move_toward(move_direction * target_speed, acceleration * delta)
+	else:
+		current_velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		
+	velocity = current_velocity
+	move_and_slide()
+	
+func handle_action() -> void:
+	if Input.is_action_just_pressed("action") and not is_action:
+		is_action = true
+		action_timer.start()
+		var nearest_body = get_nearest_harvestable_body()
+		if nearest_body:
+			nearest_body.take_damage(1)
+		else:
+			for body in action_area.get_overlapping_bodies():
+				body.take_damage(1)
+
+func get_nearest_harvestable_body() -> Node:
+	var nearest_body = null
+	var min_distance = 9999
+	var player_pos = global_position
+	for body in action_area.get_overlapping_bodies():
+		if body.is_in_group("crop"):
+			var distance = player_pos.distance_to(body.global_position)
+			if distance < min_distance:
+				min_distance = distance
+				nearest_body = body
+	return nearest_body
+
+func _on_action_timeout() -> void:
+	is_action = false
+
+func _on_range_body_entered(_body: Node2D) -> void:
+	pass # Replace with function body.
+
+func _on_range_body_exited(_body: Node2D) -> void:
+	pass # Replace with function body.
