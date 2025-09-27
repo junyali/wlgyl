ca2988dcce920d62a3224ab7631abc850d330fbc
JoshdRod <joshrod1089@gmail.com>
2025-09-27 18:34:32 +0100

    Add properties to update score module

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+@onready var popularity: int = 10
+@onready var price: int = 10
+@onready var whimsy: int = 10
 
+@onready var popularities = {}
+@onready var prices = {}
+@onready var whimsies = {}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +15,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+# Takes in a lemon, updates score
+func update_scores(lemon) -> void:
+	# Popularity
+	popularity += popularities[lemon.type]
+	# Price
+	price += prices[lemon.type]
+	# Whimsy
+	whimsy += whimsies[lemon.type]
+
+	
+	
